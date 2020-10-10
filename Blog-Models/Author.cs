a4fb9ea31bbf05b39fdc1794f8af82af2b8bc3f3
omar sawy <46130283+omarsawy123@users.noreply.github.com>
2020-10-10 20:14:25 +0200

    Added data notations

--- Blog-Models/Author.cs
+++ Blog-Models/Author.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace Blog_Models
 {
     public class Author
     {
+        
         public int Id { get; set; }
+
+        [Required]
+        [MinLength(2)]
         public string Name { get; set; }
+
+        [Required]
         public Specialty Specialty { get; set; }
-        public Blog Blogs { get; set; }
+
+        [Required]
+        public IEnumerable<Blog> Blogs { get; set; }
 
     }
 }
--- Blog-Models/Blog.cs
+++ Blog-Models/Blog.cs
@@ -1,13 +1,23 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace Blog_Models
 {
     public class Blog
     {
         public int Id { get; set; }
+
+        [Required]
+        [MinLength(5)]
         public string Name { get; set; }
+
+        [Required]
         public Author Author { get; set; }
+        
+        [Required]
         public DateTime DatePublished { get; set; }
+        
+        [Required]
         public string Content { get; set; }
        
 
