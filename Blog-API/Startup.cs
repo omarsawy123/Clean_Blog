40a2c0933cd2b72b6efd643c4c0b9078d398a527
omar sawy <46130283+omarsawy123@users.noreply.github.com>
2020-10-12 22:11:07 +0200

    AddScoped() Added

--- Blog-API/Startup.cs
+++ Blog-API/Startup.cs
@@ -29,8 +29,11 @@ namespace Blog_API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            
             services.AddDbContext<AppDBContext>(opt =>
             opt.UseSqlServer(Configuration.GetConnectionString("DbConnection")));
+
+            services.AddScoped<IBlogRepository,BlogRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
